1924bdc9adcc5182697fa8e7850f0860401bd9c5
unknown <ccm.rex17@outlook.com>
2026-03-27 18:09:17 +0800

    Add second-level menu item translations

--- app/routes/pages.$handle.jsx
+++ app/routes/pages.$handle.jsx
@@ -69,12 +69,42 @@ export default function Page() {
     <div className="page">
       <section id="page-header" className="page-section">
         <h2 className="section-heading">
+          {/* Top-level Items */}
           {page.title == 'About' && <><span className="en">About</span><span className="zh-TW">關於我們</span></>}
           {page.title == 'Donation' && <><span className="en">Donation</span><span className="zh-TW">捐助支持</span></>}
           {page.title == 'Membership' && <><span className="en">Membership</span><span className="zh-TW">會員申請</span></>}
           {page.title == 'Resources' && <><span className="en">Resources</span><span className="zh-TW">相關資源</span></>}
           {page.title == 'Contact' && <><span className="en">Contact</span><span className="zh-TW">聯絡我們</span></>}
 
+          {/* Second-level Items */}
+          {/* About */}
+          {page.title == 'History' && <><span className="en">History</span><span className="zh-TW">學會歷史</span></>}
+          {page.title == 'Message from President' && <><span className="en">Message from President</span><span className="zh-TW">會長寄語</span></>}
+          {page.title == 'Committee' && <><span className="en">Committee</span><span className="zh-TW">執委會</span></>}
+
+          {/* Upcoming Events */}
+          {page.title == 'Upcoming Events' && <><span className="en">Upcoming Events</span><span className="zh-TW">即將舉行活動</span></>}
+          {page.title == 'Gallery' && <><span className="en">Gallery</span><span className="zh-TW">活動花絮</span></>}
+
+          {/* Membership */}
+          {page.title == 'Research Fund' && <><span className="en">Research Fund</span><span className="zh-TW">研究資助</span></>}
+          {page.title == 'Sponsorship' && <><span className="en">Sponsorship</span><span className="zh-TW">贊助</span></>}
+
+          {/* Resources */}
+          {page.title == 'Health Information' && <><span className="en">Health Information</span><span className="zh-TW">健康資訊</span></>}
+          {page.title == "Dietitian's Corner" && <><span className="en">Dietitian's Corner</span><span className="zh-TW">營養師專欄</span></>}
+          {page.title == 'Exercise Tips' && <><span className="en">Exercise Tips</span><span className="zh-TW">運動小貼士</span></>}
+          {page.title == 'Exposure' && <><span className="en">Exposure</span><span className="zh-TW">媒體報導</span></>}
+          {page.title == 'EOSS Calculator' && <><span className="en">EOSS Calculator</span><span className="zh-TW">EOSS 計算機</span></>}
+          
+          {/* Else */}
+          {![
+            'About', 'Donation', 'Membership', 'Resources', 'Contact', 
+            'History', 'Message from President', 'Committee', 
+            'Upcoming Events', 'Gallery', 'Research Fund', 'Sponsorship', 
+            'Health Information', "Dietitian's Corner", 'Exercise Tips', 
+            'Exposure', 'EOSS Calculator', ''
+          ].includes(page.title) && <span>{page.title}</span>}
         </h2>
         {/* <p>Page handle: {page.handle}</p> */}
         <article className="page-description" dangerouslySetInnerHTML={{__html: page.body}} />
